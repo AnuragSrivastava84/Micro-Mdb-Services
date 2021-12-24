91940c0da5f67f20763095ee17078d8152ef6fed
AnuragSrivastava84 <anurag1984.srivas@gmail.com>
2021-12-24 14:28:12 +0800

    Changes to configure circuit breaker properties
    * Configure the timeout property for the item info service
    * Configure the request volume threshold property for the item info service
    * Configure the error threshold percentage property for the item info service
    * Configure the sleep window property for the item info service

--- micro-catalog-services/src/main/java/io/microcatalogservices/service/ItemInfoService.java
+++ micro-catalog-services/src/main/java/io/microcatalogservices/service/ItemInfoService.java
@@ -1,6 +1,7 @@
 package io.microcatalogservices.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.microcatalogservices.model.ItemInfo;
 import io.microcatalogservices.model.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,13 @@ public class ItemInfoService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackItemInfo")
+    @HystrixCommand(fallbackMethod = "getFallbackItemInfo",
+    commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value = "5"),
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
+    })
     public ItemInfo getItemInfo(Rating rating) {
         return restTemplate.getForObject("http://micro-info-service/item/"+ rating.getItemId(), ItemInfo.class);
     }
