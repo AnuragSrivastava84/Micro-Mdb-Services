3076c66e9c593dd02e5505ec6c6881f6f712f97a
AnuragSrivastava84 <anurag1984.srivas@gmail.com>
2021-12-24 14:57:42 +0800

    Set up example set for bulk-head pattern
    * Set up properties for the bulk-head pattern in the user service
    and commented that out, just to show the example of the setup

--- micro-catalog-services/src/main/java/io/microcatalogservices/service/UserRatingService.java
+++ micro-catalog-services/src/main/java/io/microcatalogservices/service/UserRatingService.java
@@ -1,6 +1,7 @@
 package io.microcatalogservices.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.microcatalogservices.model.Rating;
 import io.microcatalogservices.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,15 @@ public class UserRatingService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackUserRating"
+            /*,
+            threadPoolKey = "getUserRating",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")
+            }*/
+    )
     public UserRating getUserRating(String userId) {
         return restTemplate.getForObject("http://micro-rating-service/rating/user/"+ userId, UserRating.class);
     }
