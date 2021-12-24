e27a7fa581606a2a8b35c64a5c6ff7758e21943e
AnuragSrivastava84 <anurag1984.srivas@gmail.com>
2021-12-24 13:02:34 +0800

    Changes to configure circuit breaker
    * Add the netflix hystrix dependency in the catalog service
    * Add the fallback scenario for retrieving the default catalog list

--- micro-catalog-services/src/main/java/io/microcatalogservices/controller/MicroCatalogController.java
+++ micro-catalog-services/src/main/java/io/microcatalogservices/controller/MicroCatalogController.java
@@ -1,5 +1,6 @@
 package io.microcatalogservices.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.microcatalogservices.model.CatalogItem;
 import io.microcatalogservices.model.ItemInfo;
 import io.microcatalogservices.model.UserRating;
@@ -9,22 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
-public class MicroMdbController {
+public class MicroCatalogController {
 
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         UserRating userRating = restTemplate.getForObject("http://micro-rating-service/rating/user/"+userId, UserRating.class);
         return userRating.getRatingList().stream().map(rating -> {
             ItemInfo itemInfo = restTemplate.getForObject("http://micro-info-service/item/"+rating.getItemId(), ItemInfo.class);
             return new CatalogItem(itemInfo.getName(), "Description", rating.getRating());
         }).collect(Collectors.toList());
+    }
+
+    private List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId){
+        return Arrays.asList(new CatalogItem("No Item","No item found",0));
     }
 }
